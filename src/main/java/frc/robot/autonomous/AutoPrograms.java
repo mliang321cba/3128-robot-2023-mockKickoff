ff79b3a79cef52cf491e48e5f2aa3f98c76ca172
Mason-Lam <97353903+Mason-Lam@users.noreply.github.com>
2023-11-17 15:25:50 -0800

    Initial Commit

---
+++ src/main/java/frc/robot/autonomous/AutoPrograms.java
@@ -0,0 +1,10 @@
+package frc.robot.autonomous;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class AutoPrograms {
+    
+    public static CommandBase getAutonomousCommand() {
+        return null;
+    }
+}
